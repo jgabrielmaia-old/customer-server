05539b9a30a174b51ed108691388b793cb02baca
J. Gabriel Maia <jonagabriel@gmail.com>
2020-09-15 22:59:36 -0300

    Adding Swagger to API

--- CustomerServer/Startup.cs
+++ CustomerServer/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace CustomerServer
 {
@@ -35,6 +36,13 @@ namespace CustomerServer
             services.AddSingleton<CustomerService>();
 
             services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing());
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customers API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -55,6 +63,12 @@ namespace CustomerServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customers API V1");
             });
         }
     }
